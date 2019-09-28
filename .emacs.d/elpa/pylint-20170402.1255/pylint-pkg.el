8d18d800dd2d3e29a857bf88ab1a3d7292210dbd
gdhameeja <gaurav@melvault.com>
2019-09-28 16:07:45 +0530

    Added emacs.d and ido-vertical-mode

---
+++ .emacs.d/elpa/pylint-20170402.1255/pylint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pylint" "20170402.1255" "minor mode for running `pylint'" 'nil :commit "298a22d96ca512ab3910303ae8f2913e608d0a64" :keywords '("languages" "python") :authors '(("Ian Eure" . "ian.eure@gmail.com")) :maintainer '("Jonathan Kotta" . "jpkotta@gmail.com"))
---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,8 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-stash nil))
